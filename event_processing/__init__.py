132e66324e29b282b0e9c991ce3aa23cb1130211
Hal Smith <hal@schnibb.com>
2020-09-29 15:00:25 +0100

    Queue is now created and closed during run.

--- event_processing/__init__.py
+++ event_processing/__init__.py
@@ -2,3 +2,5 @@ from event_processing.main import EventProcessing
 
 if __name__ == "__main__":
     processing = EventProcessing()
+    processing.run()
+    print(processing.locations_list)
--- event_processing/main.py
+++ event_processing/main.py
@@ -3,39 +3,49 @@ import json
 
 import boto3
 
-from botocore.config import Config
+LOCATION_FILE_NAME = 'locations.json'
 
 
 class EventProcessing:
 
     def __init__(self):
+        params = self.parse_input_args()
+        self.aws_id = params.aws_public
+        self.aws_key = params.aws_secret
+        self.bucket_name = params.bucket_name
+
+        self.bucket = self.get_bucket()
+
+        self.locations_list = []
+
+    def run(self):
+        self.bucket.download_file(LOCATION_FILE_NAME, LOCATION_FILE_NAME)
+
+        self.populate_locations_dict()
+
+        sqs_client = boto3.client('sqs')
+
+        create_queue_response = sqs_client.create_queue(QueueName='first_queue')
+
+        queue_url = create_queue_response['QueueUrl']
+
+        sqs_client.delete_queue(QueueUrl=queue_url)
+
+    def populate_locations_dict(self):
+        with open(LOCATION_FILE_NAME, 'r') as location_file:
+            self.locations_list.extend(json.loads(location_file.read()))
+
+    def get_bucket(self):
+        session = boto3.Session(
+            aws_access_key_id=self.aws_id,
+            aws_secret_access_key=self.aws_key
+        )
+        return session.resource('s3').Bucket(self.bucket_name)
+
+    @staticmethod
+    def parse_input_args():
         parser = argparse.ArgumentParser()
         parser.add_argument("aws_public")
         parser.add_argument("aws_secret")
-        # parser.add_argument('-bucket_name', type=str)
-        args = parser.parse_args()
-        # my_config = Config()
-        # print(my_config)
-        self.session = boto3.Session(
-            aws_access_key_id=args.aws_public,
-            aws_secret_access_key=args.aws_secret
-        )
-        #
-        s3 = self.session.resource('s3')
-        bucket = s3.Bucket('eventprocessing-altran-locationss3bucket-1ub1fsm0jlky7')
-
-        # s3_obj = s3.Object('locations.json', 'locations.json').get()
-        # bucket.d
-
-        # file_as_string = bucket.get_object(Key='locations.json')['Body'].read().decode('utf-8')
-
-        # print(file_as_string)
-
-        bucket.download_file('locations.json', 'locations.json')
-
-
-        # file_string = None
-        # with open('locations.json', 'r') as location_file:
-        #     file_string = json.loads(location_file.read())
-        #
-        # print(file_string)
+        parser.add_argument("bucket_name")
+        return parser.parse_args()
