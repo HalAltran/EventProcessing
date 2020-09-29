23d1a00d35837fbc270f8e9390e36c6192127837
Hal Smith <hal@schnibb.com>
2020-09-29 16:56:18 +0100

    SQS gets SNS message and prints it.

--- event_processing/main.py
+++ event_processing/main.py
@@ -13,6 +13,7 @@ class EventProcessing:
         self.aws_id = params.aws_public
         self.aws_key = params.aws_secret
         self.bucket_name = params.bucket_name
+        self.sns_topic_name = params.sns_topic_name
 
         self.bucket = self.get_bucket()
 
@@ -23,11 +24,52 @@ class EventProcessing:
 
         self.populate_locations_dict()
 
-        sqs_client = boto3.client('sqs')
+        # set up sqs client and create queue
+
+        sqs_client = boto3.client('sqs', region_name='eu-west-1')
 
         create_queue_response = sqs_client.create_queue(QueueName='first_queue')
-
         queue_url = create_queue_response['QueueUrl']
+        queue_arn = sqs_client.get_queue_attributes(
+            QueueUrl=queue_url,
+            AttributeNames=['QueueArn']
+        )['Attributes']['QueueArn']
+
+        # add permissions
+
+        policy_document = {
+            'Version': '2012-10-17',
+            'Statement': [{
+                'Sid': f'allow-subscription-{self.sns_topic_name}',
+                'Effect': 'Allow',
+                'Principal': {'AWS': '*'},
+                'Action': 'SQS:SendMessage',
+                'Resource': f'{queue_arn}',
+                'Condition': {
+                    'ArnEquals': {'aws:SourceArn': f'{self.sns_topic_name}'}
+                }
+            }]
+        }
+
+        policy_json = json.dumps(policy_document)
+
+        sns_client = boto3.client('sns', region_name='eu-west-1')
+
+        sqs_client.set_queue_attributes(
+            QueueUrl=queue_url,
+            Attributes={'Policy': policy_json}
+        )
+
+        subscribe_request = sns_client.subscribe(
+            TopicArn=self.sns_topic_name,
+            Protocol='sqs',
+            Endpoint=queue_arn,
+            ReturnSubscriptionArn=True
+        )
+
+        print(sqs_client.receive_message(QueueUrl=queue_url))
+
+        sns_client.unsubscribe(SubscriptionArn=subscribe_request['SubscriptionArn'])
 
         sqs_client.delete_queue(QueueUrl=queue_url)
 
@@ -38,7 +80,8 @@ class EventProcessing:
     def get_bucket(self):
         session = boto3.Session(
             aws_access_key_id=self.aws_id,
-            aws_secret_access_key=self.aws_key
+            aws_secret_access_key=self.aws_key,
+            region_name='eu-west-1'
         )
         return session.resource('s3').Bucket(self.bucket_name)
 
@@ -48,4 +91,5 @@ class EventProcessing:
         parser.add_argument("aws_public")
         parser.add_argument("aws_secret")
         parser.add_argument("bucket_name")
+        parser.add_argument("sns_topic_name")
         return parser.parse_args()
